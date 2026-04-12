b2ba4cfaddbcf16417e6e30177c873e93950caf1
KB1T3 <aminukbite@gmail.com>
2026-04-12 11:31:35 +0100

    First Commit

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import '@/global.css';
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center bg-background'>
+      <Text className='text-xl font-bold text-success'>
+        Welcome to Nativewind!
+      </Text>
+      <Link
+        href='/(auth)/sign-up'
+        className='mt-4 rounded bg-primary px-4 py-2 text-white'
+      >
+        Go to Sign Up
+      </Link>
+      <Link
+        href='/(auth)/sign-in'
+        className='mt-4 rounded bg-primary px-4 py-2 text-white'
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href='/subscriptions/spotify'
+        className='mt-4 rounded bg-primary px-4 py-2 text-white'
+      >
+        Spotify Subscription
+      </Link>
+      <Link
+        href={{ pathname: '/subscriptions/[id]', params: { id: 'claude' } }}
+        className='mt-4 rounded bg-primary px-4 py-2 text-white'
+      >
+        Claude Max Subscription
+      </Link>
+    </View>
+  );
+}
